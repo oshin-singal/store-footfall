8c6a8b44f536a1ed43395d66a77013737a3e58e6
Oshin Singal <136887346+oshin-singal@users.noreply.github.com>
2023-11-21 15:59:58 +0530

    Create encryption.py

---
+++ encryption.py
@@ -0,0 +1,27 @@
+'''
+FUNCTION
+Will accept data from the transmission layer.
+Will decrypt it (to be added later)
+Will encrypt it
+Will push to db
+'''
+
+'''
+DURATION
+Initialised at the start
+Automation and restarting to be implemented
+'''
+
+
+'''
+CAUTION
+Edit column types to handle encrypted data
+Keys to not be hosted
+Decryption to be created
+Add transmission encryption
+'''
+
+'''
+OPEN QUESTIONS
+Augmenting from multiple sources
+'''
